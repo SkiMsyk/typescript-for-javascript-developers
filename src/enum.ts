3b1e7a5e0c286c7b7a877aee15c7e03f76bd87af
SkiMsyk <Sakai.Masayuki@keio.jp>
2022-09-12 17:50:01 +0900

    finish section-02

---
+++ src/enum.ts
@@ -0,0 +1,35 @@
+export {};
+
+enum Months {
+  January = 1, // set initial number as 1
+  February,
+  March,
+  April,
+  May,
+  Jun,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December,
+}
+
+console.log(Months.January);
+console.log(Months.February);
+enum COLORS {
+  RED = "#FF0000",
+  WHITE = "#FFFFFF",
+  GREEN = "#008000",
+  BLUE = "#0000FF",
+  BLACK = "#000000",
+}
+console.log(COLORS.RED);
+console.log(COLORS.BLACK);
+
+enum COLORS {
+  YELLOW = "#FFFF00",
+  GRAY = "#808080",
+}
+
+console.log(COLORS.YELLOW);
--- src/intersection.ts
+++ src/intersection.ts
@@ -1,1 +1,30 @@
 export {};
+
+type Pitcher1 = {
+  throwingSpeed: number;
+};
+
+type Batter1 = {
+  battingAverage: number;
+};
+
+const Sasaki: Pitcher1 = {
+  throwingSpeed: 154,
+};
+
+const Ochiai: Batter1 = {
+  battingAverage: 0.367,
+};
+
+// intersection
+type TwoWayPlayer = Pitcher1 & Batter1;
+// equals
+// {
+//   throwingSpeed: number;
+//   battingAverage: number;
+// }
+
+const Otani: TwoWayPlayer = {
+  throwingSpeed: 165,
+  battingAverage: 0.286,
+};
