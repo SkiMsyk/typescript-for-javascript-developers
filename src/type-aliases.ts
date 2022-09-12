8fd263baa2ed7fe3961affa7a102a122ec78e3db
SkiMsyk <Sakai.Masayuki@keio.jp>
2022-09-12 17:51:04 +0900

    fix type in type-aliases.ts

--- src/type-aliases.ts
+++ src/type-aliases.ts
@@ -2,24 +2,24 @@ export {};
 
 type Mojiretsu = string;
 
-const fooString: Mojiretsu = 'This type is Mojiretsu';
+const fooString: Mojiretsu = "This type is Mojiretsu";
 console.log(fooString);
 
 const example1 = {
-  name: 'Ham',
-  age: 43
+  name: "Ham",
+  age: 43,
 };
 console.log(example1);
 
 type Profile = {
-    name: string;
-    age: number;
-}
+  name: string;
+  age: number;
+};
 
 const example2: Profile = {
-  name: 'Joe',
-  age: 50
-}
+  name: "Joe",
+  age: 50,
+};
 console.log(example2);
 
-type Profile2 = typeof example1;
+type Profile2 = typeof example1; // 既にあるオブジェクトから型を流用する方法
